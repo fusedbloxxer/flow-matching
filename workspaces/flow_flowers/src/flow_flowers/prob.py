ae694235dc7fded6247084078f7a516ddd29de68
Andrei Ionescu <voidwarden@yahoo.com>
2025-06-28 23:47:26 +0300

    Add sampleable

--- workspaces/flow_flowers/src/flow_flowers/prob.py
+++ workspaces/flow_flowers/src/flow_flowers/prob.py
@@ -1,18 +1,54 @@
 from abc import ABC, abstractmethod
 from dataclasses import dataclass
-from typing import override
+from typing import Any, Callable, Optional, Tuple, cast, override
+
+import torch
 
 from torch import Tensor
+from torch.utils.data import Dataset
 
 
-@dataclass
+@dataclass(kw_only=True)
 class Sampleable(ABC):
+    device: Optional[torch.device] = None
+
     @abstractmethod
     def sample(self, batch_size: int) -> Tensor:
         raise NotImplementedError()
 
 
-@dataclass
+@dataclass(kw_only=True)
+class GaussianSampleable(Sampleable):
+    size: Tuple
+
+    def sample(self, batch_size: int) -> Tensor:
+        x = torch.randn((batch_size, *self.size))
+        if self.device:
+            x = x.to(self.device)
+        return x
+
+
+@dataclass(kw_only=True)
+class DatasetSampleable(Sampleable):
+    data: Dataset
+    map_entry: Optional[Callable[[Any], Tensor]] = None
+    map_batch: Optional[Callable[[Tensor], Tensor]] = None
+
+    def sample(self, batch_size: int) -> Tensor:
+        size = len(cast(Any, self.data))
+        indices = torch.randperm(size)[:batch_size].tolist()
+
+        x = []
+        for i in indices:
+            x_i = self.data[i]
+            x.append(self.map_entry(x_i) if self.map_entry else x_i)
+
+        x = torch.stack(x, dim=0).to(self.device)
+        x = self.map_batch(x) if self.map_batch else x
+        return x
+
+
+@dataclass(kw_only=True)
 class ProbPath(ABC):
     p_init: Sampleable
     p_data: Sampleable
@@ -26,12 +62,12 @@ class ProbPath(ABC):
         raise NotImplementedError()
 
     def cond_prob_path(self, *, x_1: Tensor, t: Tensor) -> Tensor:
-        x_0 = self.p_init.sample(t.size(0))
+        x_0 = self.p_init.sample(x_1.size(0))
         x_t = self.prob_path_flow(x_0=x_0, x_1=x_1, t=t)
         return x_t
 
-    def marg_prob_path(self, *, t: Tensor) -> Tensor:
-        x_1 = self.p_data.sample(t.size(0))
+    def marg_prob_path(self, *, n: int, t: Tensor) -> Tensor:
+        x_1 = self.p_data.sample(n)
         x_t = self.cond_prob_path(x_1=x_1, t=t)
         return x_t
 
@@ -39,7 +75,7 @@ class ProbPath(ABC):
         return self.cond_vect_field(x_1=x_1, x_t=x_t, t=t)
 
 
-@dataclass
+@dataclass(kw_only=True)
 class OTProbPath(ProbPath):
     def cond_vect_field(self, *, x_1: Tensor, x_t: Tensor, t: Tensor) -> Tensor:
         return (x_1 - x_t) * (1.0 - t) ** -1.0
