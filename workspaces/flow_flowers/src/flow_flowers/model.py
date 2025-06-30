c2a238ad3142ab4ddf0b6aa3d303cc5efeb671b7
Andrei Ionescu <voidwarden@yahoo.com>
2025-06-30 23:57:42 +0300

    Add some building blocks for DiCo

--- workspaces/flow_flowers/src/flow_flowers/model.py
+++ workspaces/flow_flowers/src/flow_flowers/model.py
@@ -2,6 +2,7 @@ from typing import cast
 
 import torch
 import torch.nn as nn
+import torch.nn.functional as F
 
 from diffusers.models.autoencoders.autoencoder_dc import AutoencoderDC
 from diffusers.models.autoencoders.vae import DecoderOutput, EncoderOutput
@@ -12,8 +13,8 @@ class AutoEncoder(nn.Module):
     std: Tensor
     mu: Tensor
 
-    def __init__(self, *args, id: str, mu: float = 0.0, std: float = 1.0, **kwargs) -> None:
-        super().__init__(*args, **kwargs)
+    def __init__(self, *, id: str, mu: float = 0.0, std: float = 1.0, **kwargs) -> None:
+        super().__init__()
 
         # Used to re-scale latents
         self.register_buffer("std", torch.tensor(std))
@@ -35,6 +36,60 @@ class AutoEncoder(nn.Module):
         x_dec = self.dc_ae.decode(x)
         x_out = cast(DecoderOutput, x_dec).sample
         return x_out
+
+
+class LabelEmbedder(nn.Module):
+    def __init__(self, *, in_dim: int, emb_dim: int, **kwargs) -> None:
+        super().__init__()
+        self.pad_idx = 0
+        self.num_emb = in_dim + 1
+        self.embd_proj = nn.Embedding(self.num_emb, emb_dim, self.pad_idx)
+
+    def forward(self, x: Tensor) -> Tensor:
+        return self.embd_proj(x)
+
+
+class MLP(nn.Module):
+    def __init__(self, *, in_dim: int, h_dim: int, out_dim: int, layers: int, **kwargs) -> None:
+        super().__init__()
+        self.mlp = nn.Sequential()
+
+        for i in range(layers):
+            if i == 0:
+                self.mlp.extend(
+                    [
+                        nn.Linear(in_dim, h_dim),
+                        nn.SiLU(),
+                    ]
+                )
+                continue
+            if i == layers - 1:
+                self.mlp.extend(
+                    [
+                        nn.Linear(h_dim, out_dim),
+                        nn.SiLU(),
+                    ]
+                )
+                continue
+            self.mlp.extend(
+                [
+                    nn.Linear(h_dim, h_dim),
+                    nn.SiLU(),
+                ]
+            )
+
+    def forward(self, x: Tensor) -> Tensor:
+        return self.mlp(x)
+
+
+class CompactChannelAttn(nn.Module):
+    def __init__(self, *, pool_kernel_size: int, in_dim: int, out_dim: int, **kwargs) -> None:
+        super().__init__()
+        self.gap = nn.AvgPool2d(pool_kernel_size, stride=1)
+        self.conv = nn.Conv2d(in_dim, out_dim, kernel_size=1, stride=1, padding=0)
+
+    def forward(self, x: Tensor) -> Tensor:
+        return x * F.sigmoid(self.conv(self.gap(x)))
 
 
 __all__ = ["AutoEncoder"]
