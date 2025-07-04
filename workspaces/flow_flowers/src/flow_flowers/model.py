b4be707875035f37db9734fd929fa5cb3b80d428
Andrei Ionescu <voidwarden@yahoo.com>
2025-07-05 00:00:36 +0300

    Add CCA, ConvModule, AdaLN

--- workspaces/flow_flowers/src/flow_flowers/model.py
+++ workspaces/flow_flowers/src/flow_flowers/model.py
@@ -1,4 +1,4 @@
-from typing import cast
+from typing import TypedDict, cast
 
 import torch
 import torch.nn as nn
@@ -52,6 +52,18 @@ class LabelEmbedder(nn.Module):
 class MLP(nn.Module):
     def __init__(self, *, in_dim: int, h_dim: int, out_dim: int, layers: int, **kwargs) -> None:
         super().__init__()
+        assert layers >= 1, "MLP must have at least one layer"
+        assert layers > 1 or h_dim != out_dim, "MLP must have h_dim == out_dim when layers == 1"
+
+        if layers == 1:
+            self.mlp = nn.Sequential(
+                *[
+                    nn.Linear(in_dim, out_dim),
+                    nn.SiLU(),
+                ]
+            )
+            return
+
         self.mlp = nn.Sequential()
 
         for i in range(layers):
@@ -83,13 +95,79 @@ class MLP(nn.Module):
 
 
 class CompactChannelAttn(nn.Module):
-    def __init__(self, *, pool_kernel_size: int, in_dim: int, out_dim: int, **kwargs) -> None:
+    def __init__(self, *, channels: int, **kwargs) -> None:
         super().__init__()
-        self.gap = nn.AvgPool2d(pool_kernel_size, stride=1)
-        self.conv = nn.Conv2d(in_dim, out_dim, kernel_size=1, stride=1, padding=0)
+        self.gap = nn.AdaptiveAvgPool2d(output_size=(1, 1))
+        self.conv = nn.Conv2d(channels, channels, kernel_size=1)
 
     def forward(self, x: Tensor) -> Tensor:
-        return x * F.sigmoid(self.conv(self.gap(x)))
+        gap_features: Tensor = self.gap(x)
+        attn_weights: Tensor = F.sigmoid(self.conv(gap_features))
+        return x * attn_weights
+
+
+class ConvModule(nn.Module):
+    def __init__(self, *, channels: int, **kwargs) -> None:
+        super().__init__()
+        self.conv_p1 = nn.Conv2d(channels, channels, kernel_size=1)
+        self.conv_d = nn.Conv2d(channels, channels, kernel_size=3, padding=1, groups=channels)
+        self.cca = CompactChannelAttn(channels=channels)
+        self.conv_p2 = nn.Conv2d(channels, channels, kernel_size=1)
+
+    def forward(self, x: Tensor) -> Tensor:
+        h: Tensor = x
+        h = self.conv_p1(x)
+        h = self.conv_d(x)
+        h = F.silu(h)
+        h = self.cca(x)
+        h = self.conv_p2(x)
+        return h
+
+
+class ModulateOutput(TypedDict):
+    scale: Tensor
+    shift: Tensor
+    gate: Tensor
+
+
+class AdaLNOutput(TypedDict):
+    msa: ModulateOutput
+    mlp: ModulateOutput
+
+
+class AdaLN(nn.Module):
+    def __init__(self, *, h_dim: int, **kwargs) -> None:
+        super().__init__()
+        self.mlp = nn.Sequential(
+            *[
+                nn.SiLU(),
+                nn.Linear(h_dim, 6 * h_dim),
+                nn.Unflatten(dim=1, unflattened_size=(6, h_dim)),
+            ]
+        )
+
+    def forward(self, x: Tensor) -> AdaLNOutput:
+        h = self.mlp(x)
+        h = torch.unbind(h, dim=1)
+        shift_msa, scale_msa, gate_msa = h[:3]
+        shift_mlp, scale_mlp, gate_mlp = h[3:]
+        msa: ModulateOutput = {"shift": shift_msa, "scale": scale_msa, "gate": gate_msa}
+        mlp: ModulateOutput = {"shift": shift_mlp, "scale": scale_mlp, "gate": gate_mlp}
+        return {"msa": msa, "mlp": mlp}
+
+    @staticmethod
+    def modulate(x: Tensor, shift: Tensor, scale: Tensor) -> Tensor:
+        return x * (1 + scale.unsqueeze(1)) + shift.unsqueeze(1)
+
+
+class DiCo(nn.Module):
+    def __init__(self, *, in_dim: int, d_dim: int, out_dim: int, **kwargs) -> None:
+        super().__init__()
+
+        self.in_proj = nn.Conv2d(in_dim, d_dim, kernel_size=3, stride=1, padding=1)
+
+    def forward(self, x_t: Tensor, t: Tensor, y: Tensor) -> Tensor:
+        return torch.zeros()
 
 
 __all__ = ["AutoEncoder"]
