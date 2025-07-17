14bc97b5c14a541747847dea5a3e75ed474e6c9d
Andrei Ionescu <voidwarden@yahoo.com>
2025-07-18 00:27:45 +0300

    feat(flow_anime): add more parameters

--- workspaces/flow_anime/src/flow_anime/args.py
+++ workspaces/flow_anime/src/flow_anime/args.py
@@ -1,4 +1,5 @@
-from typing import Annotated
+from pathlib import Path
+from typing import Annotated, Literal
 
 from msgspec import Struct
 from tyro.conf import arg, subcommand
@@ -10,8 +11,28 @@ class WandbArgs(Struct):
     # The name of the experiment
     exp_name: str
 
+    # The name of the run
+    run_name: str
+
+    # Log interval in steps
+    interval: int = 1
+
 
 _WandbArgs = Annotated[WandbArgs, subcommand(name="wandb")]
+
+
+class DatasetArgs(Struct):
+    """Arguments for dataset"""
+
+    # The path to the dataset directory
+    path: Path
+
+
+class DanbooruDatasetArgs(DatasetArgs):
+    """Arguments for Danbooru dataset"""
+
+
+_DanbooruDatasetArgs = Annotated[DanbooruDatasetArgs, arg(name="danbooru")]
 
 
 class SDXLAutoEncoderArgs(Struct, tag="sdxl"):
@@ -61,14 +82,68 @@ class T5CLIPTextEncoderArgs(Struct, tag="t5clip"):
 _T5CLIPTextEncoderArgs = Annotated[T5CLIPTextEncoderArgs, subcommand(name="t5clip")]
 
 
+class TrainParamsArgs(Struct):
+    """Arguments for training parameters"""
+
+    # The learning rate of the optimizer
+    lr: float = 1e-4
+
+    # The batch size of the training
+    bs: int = 8
+
+    # The number of epochs
+    epochs: int | None = None
+
+    # The number of steps
+    steps: int | None = None
+
+    # Evaluate split ratio
+    eval_split: float | None = 0.1
+
+    # Evaluate every n steps
+    eval_every: int | None = 10
+
+    # Checkpoint name
+    ckpt_name: str | None = None
+
+    # Checkpoint directory
+    ckpt_dir: Path | None = None
+
+    # Checkpoint every n steps
+    ckpt_every: int | None = 10
+
+    # Resume from checkpoint
+    ckpt_resume: bool = False
+
+    def __post_init__(self):
+        if self.epochs is None and self.steps is None:
+            raise ValueError("epochs or steps must be set")
+        if self.epochs is not None and self.steps is not None:
+            raise ValueError("epochs and steps cannot be both set")
+        if self.ckpt_resume and (self.ckpt_name is None or self.ckpt_dir is None or not self.ckpt_dir.exists()):
+            raise ValueError("ckpt_name and ckpt_dir must be set if ckpt_resume is True")
+
+
+_TrainParametersArgs = Annotated[TrainParamsArgs, arg(name="train")]
+
+
 class TrainArgs(Struct, kw_only=True):
     """Arguments for training"""
+
+    # The devices to be used
+    device: Literal["cpu", "cuda:0", "cuda:1"] = "cuda:0"
 
     # Enable verbose logging
     verbose: bool = True
 
     # The seed of the experiment
     seed: int = 42
+
+    # Dataset used for training
+    data: _DanbooruDatasetArgs
+
+    # Training parameters
+    train: _TrainParametersArgs
 
     # Tracking provider for experiments
     track: _WandbArgs | None = None
